06e202abca0cf01996b7c848de273a523c19e392
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-07-08 12:08:40 +0200

    Feat(Api): create Video model

--- content/models.py
+++ content/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import date
 
 
+class Video(models.model):
+    create_at = models.DateField(default=date.today)
+    title = models.CharField(max_length=80)
+    description = models.CharField(max_length=400)
+    video_file = models.FileField(upload_to="videos", blank=True, null=True)
