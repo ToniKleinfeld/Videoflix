f83c1465f25942bbba81b9ee3181db289a774207
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-07-10 10:55:13 +0000

    Refactor(Api): Model Video

--- content/models.py
+++ content/models.py
@@ -5,5 +5,15 @@ from datetime import date
 class Video(models.Model):
     create_at = models.DateField(default=date.today)
     title = models.CharField(max_length=80)
-    description = models.CharField(max_length=400)
+    description = models.TextField(max_length=400)
     video_file = models.FileField(upload_to="videos", blank=True, null=True)
+    thumbnail_url = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=40, default="None")
+
+    class Meta:
+        verbose_name = "Video"
+        verbose_name_plural = "Videos"
+        ordering = ["-create_at"]
+
+    def __str__(self):
+        return self.title
