c22a0716d4b93b904df3bb7843a63460ea43a5dd
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-07-11 12:31:40 +0000

    refactor: Add new comands from tasks when create or delte a new Video

--- content/signals.py
+++ content/signals.py
@@ -1,14 +1,20 @@
+import os
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 from django.core.files.storage import default_storage
 from django.db import transaction
-
-import django_rq
+from django.conf import settings
 
 from .models import Video
-from content.tasks import generate_video_thumbnail
-import logging
+from content.tasks import generate_video_thumbnail, process_video_task
 from urllib.parse import unquote
+from datetime import timedelta
+
+import django_rq
+import shutil
+import logging
+import ffmpeg
+
 
 logger = logging.getLogger(__name__)
 
@@ -19,9 +25,19 @@ def video_post_save(sender, instance, created, **kwargs):
     Signal is triggered after a video has been saved
     """
     if created and instance.video_file:
-        logger.info(f"New Video {instance.title} uploaded, start thumbnail generation")
+        try:
+            probe = ffmpeg.probe(instance.video_file.path)
+            duration_seconds = float(probe["format"]["duration"])
+            instance.duration = timedelta(seconds=duration_seconds)
+            instance.save(update_fields=["duration"])
+        except Exception as e:
+            logger.error(f"Error when extracting the duration: {e}")
 
+        logger.info(f"New Video {instance.title} uploaded, start thumbnail generation")
         transaction.on_commit(lambda: django_rq.enqueue(generate_video_thumbnail, instance.id))
+
+        logger.info(f"New Video {instance.title} uploaded, start Generating different resolutions")
+        transaction.on_commit(lambda: django_rq.enqueue(process_video_task, instance.id))
 
     elif not created and instance.video_file:
         pass
@@ -30,7 +46,7 @@ def video_post_save(sender, instance, created, **kwargs):
 @receiver(post_delete, sender=Video)
 def video_post_delete(sender, instance, **kwargs):
     """
-    Delte file from filesystem
+    Delte files from filesystem
     when corresponding 'Video' object is delted.
     """
 
@@ -49,4 +65,18 @@ def video_post_delete(sender, instance, **kwargs):
             default_storage.delete(thumbnail_path)
             logger.info(f"Thumbnail {instance.title + str(instance.id)} deleted")
         except Exception as e:
-            logger.error(f"Error when deleting the thumbnail for{instance.id}: {str(e)}")
+            logger.error(f"Error when deleting the thumbnail for{instance}: {str(e)}")
+
+    cleanup_hls_files(instance.id)
+
+
+def cleanup_hls_files(video):
+    """Helpfunction delte HLS-files"""
+
+    video_dir = os.path.join(settings.MEDIA_ROOT, "hls", str(video.id))
+    if os.path.exists(video_dir):
+        try:
+            shutil.rmtree(video_dir)
+            print(f"HLS-files Video {video.title} deletet")
+        except Exception as e:
+            print(f"Error delete HLS-files: {e}")
