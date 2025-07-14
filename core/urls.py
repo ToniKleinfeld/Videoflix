2d298fac009a2515d6f848d716fa07d9fd841717
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-07-14 10:24:32 +0000

    Feat(Api): add video path to content app

--- core/urls.py
+++ core/urls.py
@@ -23,5 +23,6 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("auth_app.api.urls")),
+    path("api/video/", include("content.api.urls")),
     path("django-rq/", include("django_rq.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
