345b52321d5aa5a27125cfa6b38ecbfb3779a4da
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-07-19 14:27:37 +0000

    Refaktor(api) registration , add confirmed_passwort and validation

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -10,12 +10,19 @@ User = get_user_model()
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
+    confirmed_password = serializers.CharField(write_only=True)
     username = serializers.CharField(read_only=True)
 
     class Meta:
         model = User
-        fields = ["username", "email", "password"]
+        fields = ["username", "email", "password", "confirmed_password"]
         extra_kwargs = {"password": {"write_only": True}}
+
+    def validate_confirmed_password(self, value):
+        password = self.initial_data.get("password")
+        if password and value and password != value:
+            raise serializers.ValidationError("Passwords do not match")
+        return value
 
     def validate_email(self, value):
         if User.objects.filter(email=value).exists():
